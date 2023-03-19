57f2925242f4377d3fca5e9c102b45373b7fcec7
Nevin Ozkan <ozkannevin73@gmail.com>
2023-03-19 11:42:52 +0300

    Commit

--- GestureRegonizerApp/GestureRegonizerApp/ViewController.swift
+++ GestureRegonizerApp/GestureRegonizerApp/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+    
+    }
+    
+    @objc func changePic() {
+        var isJames = true
+        
+        if isJames == true {
+            imageView.image = UIImage(named: "Lars")
+            myLabel.text = "Lars Ulrich"
+            isJames = false
+        }else {
+            
+            imageView.image = UIImage(named: "James")
+            myLabel.text = "James Hetfield"
+            isJames = true
+      
+            
+        }
+       
+        
     }
 
 
