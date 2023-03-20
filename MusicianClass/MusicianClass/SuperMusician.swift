b1c8eb804bba2bfdc265fc51547cf2a724e0cecb
Nevin Ozkan <ozkannevin73@gmail.com>
2023-03-20 21:09:39 +0300

    Proje Oluşturuldu

--- MusicianClass/MusicianClass/SuperMusician.swift
+++ MusicianClass/MusicianClass/SuperMusician.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class SuperMusician : Musicians {
+    
+    func sing2() {
+        print("enter night")
+    }
+    
+    override func sing() {
+        super.sing()
+        print("exit light")
+    }
+}
