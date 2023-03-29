d5c3f50b27dd5f43e82df4b3db8a6df7f4187064
Nevin Ozkan <ozkannevin73@gmail.com>
2023-03-30 02:08:24 +0300

    Proje Oluşturukdu

---
+++ FoursquareClıne/Pods/Target Support Files/Pods-FoursquareClıne/Pods-FoursquareClıne-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoursquareCl_ne : NSObject
+@end
+@implementation PodsDummy_Pods_FoursquareCl_ne
+@end
---
+++ FoursquareClıne/Pods/Target Support Files/Pods-FoursquareClıne/Pods-FoursquareClıne-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FoursquareCl_neVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FoursquareCl_neVersionString[];
+
