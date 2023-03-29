c9cc33ddce2f40e648effb1f3568fddc58e2098a
Nevin Ozkan <ozkannevin73@gmail.com>
2023-03-29 23:26:52 +0300

    Projeyi Oluşturdum

--- InstaCloneeFirebase/InstaCloneeFirebase/ViewController.swift
+++ InstaCloneeFirebase/InstaCloneeFirebase/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  InstaCloneFirebase
+//  InstaCloneeFirebase
 //
-//  Created by Nevin Özkan on 25.03.2023.
+//  Created by Nevin Özkan on 27.03.2023.
 //
 
 import UIKit
