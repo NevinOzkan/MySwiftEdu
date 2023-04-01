a1d684d39f63b4cffaae0475aed9af11f4e85161
Nevin Ozkan <ozkannevin73@gmail.com>
2023-04-01 20:10:21 +0300

    Proje Hazır

---
+++ FoursquareClone/FoursquareClone/PlaceModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceModel.swift
+//  FoursquareClone
+//
+//  Created by Nevin Özkan on 31.03.2023.
+//
+
+import Foundation
---
+++ FoursquareClone/FoursquareClone/PlacesVC.swift
@@ -0,0 +1,29 @@
+//
+//  PlacesVC.swift
+//  FoursquareClone
+//
+//  Created by Nevin Özkan on 31.03.2023.
+//
+
+import UIKit
+
+class PlacesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
