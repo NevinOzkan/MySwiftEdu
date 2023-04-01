896c89c0ee5c35babb5e2617d4a6277e7452424f
Nevin Ozkan <ozkannevin73@gmail.com>
2023-04-01 20:26:45 +0300

    Proje Oluştu

--- FoursquareClone/FoursquareClone/AddPlaceVC.swift
+++ FoursquareClone/FoursquareClone/AddPlaceVC.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
-class AddPlaceVC: UIViewController {
+//var globalName = ""
+//var globalType = ""
+//var globalatmosphere = ""
 
+class AddPlaceVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var placeType: UITextField!
+    @IBOutlet weak var placeatmosphere: UITextField!
+    
+    @IBOutlet weak var placeImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        placeImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        placeImageView.addGestureRecognizer(gestureRecognizer)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        
+        if placeName.text != "" && placeType.text != "" &&  placeatmosphere.text != "" {
+            if let chosenImage = placeImageView.image{
+                let placeModel = PlaceModel.sharedInstance
+                placeModel.placeName = placeName.text!
+                placeModel.placeType = placeType.text!
+                placeModel.placeAtmosphere = placeatmosphere.text!
+                placeModel.placeImage = chosenImage
+            }
+            
+            
+        
+            performSegue(withIdentifier: "toMapVC", sender: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "PlaceName/Type/Atmosphere??", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+        }
+        
+       
     }
-    */
-
+    
+    @objc func chooseImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        placeImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- FoursquareClone/FoursquareClone/PlaceModel.swift
+++ FoursquareClone/FoursquareClone/PlaceModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import UIKit
+
+class PlaceModel {
+    static let sharedInstance = PlaceModel()
+    
+    var placeName = ""
+    var placeType = ""
+    var placeAtmosphere = ""
+    var placeImage = UIImage()
+    var placeLatitude = ""
+    var placeLongitude = ""
+    
+    private init () {}
+}
