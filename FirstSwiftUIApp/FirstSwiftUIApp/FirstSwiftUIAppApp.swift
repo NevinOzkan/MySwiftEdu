1adddf55107ae7693373f16688e8266345ec12a8
Nevin Ozkan <ozkannevin73@gmail.com>
2023-04-03 20:32:52 +0300

    SwiftUI

---
+++ FirstSwiftUIApp/FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Nevin Özkan on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstSwiftUIApp/FirstSwiftUIApp/FourthView.swift
@@ -0,0 +1,20 @@
+//
+//  FourthView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Nevin Özkan on 3.04.2023.
+//
+
+import SwiftUI
+
+struct FourthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FourthView_Previews: PreviewProvider {
+    static var previews: some View {
+        FourthView()
+    }
+}
