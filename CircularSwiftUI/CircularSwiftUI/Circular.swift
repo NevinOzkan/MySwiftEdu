eb5cb2308e7eb6743e2c155ca24e412b2bfd6edd
Nevin Ozkan <ozkannevin73@gmail.com>
2023-04-07 21:30:58 +0300

    CircularSUI

---
+++ CircularSwiftUI/CircularSwiftUI/Circular.swift
@@ -0,0 +1,20 @@
+//
+//  Circular.swift
+//  CircularSwiftUI
+//
+//  Created by Nevin Özkan on 7.04.2023.
+//
+
+import SwiftUI
+
+struct Circular: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Circular_Previews: PreviewProvider {
+    static var previews: some View {
+        Circular()
+    }
+}
---
+++ CircularSwiftUI/CircularSwiftUI/CircularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSwiftUIApp.swift
+//  CircularSwiftUI
+//
+//  Created by Nevin Özkan on 7.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
