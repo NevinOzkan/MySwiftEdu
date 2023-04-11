39d2c52674bc5b86dc67a9ea8f20ad7981e96fed
Nevin Ozkan <ozkannevin73@gmail.com>
2023-04-11 23:36:08 +0300

    Admob

--- RetainCycleExplanation/RetainCycleExplanation/Instructor.swift
+++ RetainCycleExplanation/RetainCycleExplanation/Instructor.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class Course {
+    let name: String
+    let url: URL
+    var instructor: Instructor?
+
+    init(name: String, url: URL) { self.name = name; self.url = url }
+
+    deinit {
+        print("Course \(name) deinitialized")
+    }
+}
+
+class Instructor {
+    let name: String
+    var course: Course?
+
+    init(name: String) { self.name = name }
+
+    deinit {
+        print("Instructor \(name) deinitialized")
+    }
+}
--- RetainCycleExplanation/RetainCycleExplanation/WeakInstructor.swift
+++ RetainCycleExplanation/RetainCycleExplanation/WeakInstructor.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+
+class WeakCourse {
+    let name: String
+    let url: URL
+    weak var instructor: WeakInstructor?
+
+    init(name: String, url: URL) { self.name = name; self.url = url }
+
+    deinit {
+        print("WeakCourse2 \(name) deinitialized")
+    }
+}
+
+class WeakInstructor {
+    let name: String
+    var course: WeakCourse?
+
+    init(name: String) { self.name = name }
+
+    deinit {
+        print("WeakInstructor2 \(name) deinitialized")
+    }
+}
