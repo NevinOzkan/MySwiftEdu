eda9aa5591786744cb36afbda6551df5dbfb928a
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-04 21:47:45 +0300

    Widgets

--- IOSYasamDongusu/IOSYasamDongusu/ViewController.swift
+++ IOSYasamDongusu/IOSYasamDongusu/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad çalıştı")
+       }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppearçalıştı")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear çalıştı")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear çalıştı")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear çalıştı")
     }
 
 
