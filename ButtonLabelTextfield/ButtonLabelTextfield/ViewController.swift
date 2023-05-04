17b22d82a7c3625dc7b4612652b9a809b4a6f08b
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-04 22:47:31 +0300

    OyunHazir

--- ButtonLabelTextfield/ButtonLabelTextfield/ViewController.swift
+++ ButtonLabelTextfield/ButtonLabelTextfield/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var etiket: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tikla(_ sender: Any) {
+        
+        if let alinanVeri = textField.text {
+            if let sayi = Int(alinanVeri) {
+                
+                let islem = sayi * 2
+                
+                etiket.text = String(islem)
+        }
+        }
+    }
+    
 }
 
---
+++ SayiTahminEt/SayiTahminEt/SonucEkraniVC.swift
@@ -0,0 +1,29 @@
+//
+//  SonucEkraniVC.swift
+//  SayiTahminEt
+//
+//  Created by Nevin Özkan on 4.05.2023.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
