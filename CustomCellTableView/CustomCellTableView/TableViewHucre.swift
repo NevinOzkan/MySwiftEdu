c5af8c54ca1577eedcc5f9d2e319c432dd815b0c
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-05 20:54:41 +0300

    UserDefaults

--- CustomCellTableView/CustomCellTableView/TableViewHucre.swift
+++ CustomCellTableView/CustomCellTableView/TableViewHucre.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
+protocol TableViewHucreProtocol {
+    func hucreUzerindekiButtonTiklandi(indexPath:IndexPath)
+}
+
 class TableViewHucre: UITableViewCell {
 
+    @IBOutlet weak var kisiAdLabel: UILabel!
+    
+    var hucreProtocol: TableViewHucreProtocol?
+    var indexPath:IndexPath?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,4 +29,10 @@ class TableViewHucre: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    @IBAction func buttonTikla(_ sender: Any) {
+        
+        hucreProtocol?.hucreUzerindekiButtonTiklandi(indexPath: indexPath!)
+        
+        
+    }
 }
