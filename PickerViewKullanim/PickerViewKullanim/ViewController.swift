9dcc212112d8329c82fb6c5151d54c4f4b3993e8
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-05 15:31:45 +0300

    TableView

--- PickerViewKullanim/PickerViewKullanim/ViewController.swift
+++ PickerViewKullanim/PickerViewKullanim/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var labelSonuc: UILabel!
+    
+    var ulkeler: [String] = [String]()
+    
+    var secilenUlke:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ulkeler = ["Türkiye", "Almanya", "Japonya", "Rusya", "italya" ]
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ulkeler.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
+        forComponent component: Int) -> String? {
+        return ulkeler[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,
+        inComponent component: Int) {
+        
+        labelSonuc.text = ulkeler[row]
+        
+        secilenUlke = ulkeler[row]
+        
+        
     }
 
-
+    @IBAction func goster(_ sender: Any) {
+        print(secilenUlke)
+    }
+    
 }
 
