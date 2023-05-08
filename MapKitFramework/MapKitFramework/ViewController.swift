67e8e510071fdc491fb825e7a60bd6a0aeb6756b
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-08 15:15:24 +0300

    MapKit and Location

--- MapKitFramework/MapKitFramework/ViewController.swift
+++ MapKitFramework/MapKitFramework/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let konum = CLLocationCoordinate2D(latitude: 41.0370014, longitude:
+        28.9763369)
+        
+        let span = MKCoordinateSpan (latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let bolge = MKCoordinateRegion (center: konum, span: span)
+        
+        mapView.setRegion(bolge, animated: true)
+        
+        let pin = MKPointAnnotation()
+        
+        pin.coordinate = konum
+        pin.title = "Taksim"
+        pin.subtitle = "Alt başlık"
+        
+        mapView.addAnnotation(pin)
     }
 
 
