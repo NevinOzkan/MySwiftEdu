b15c693b71643c0b0fc8284438a6013fff7f30ff
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-08 20:23:46 +0300

    Protocol

---
+++ ProtocolKullanimi/ProtocolKullanimi/DetayVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetayVC.swift
+//  ProtocolKullanimi
+//
+//  Created by Nevin Özkan on 8.05.2023.
+//
+
+import UIKit
+
+class DetayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProtocolKullanimi/ProtocolKullanimi/Protocols.swift
@@ -0,0 +1,8 @@
+//
+//  Protocols.swift
+//  ProtocolKullanimi
+//
+//  Created by Nevin Özkan on 8.05.2023.
+//
+
+import Foundation
