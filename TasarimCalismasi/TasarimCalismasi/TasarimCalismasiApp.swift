0ec98095a70c7b8d8e58c55bcef767dcf40cbe77
Nevin Ozkan <ozkannevin73@gmail.com>
2023-05-10 22:38:23 +0300

    SwiftUI

---
+++ TasarimCalismasi/TasarimCalismasi/TasarimCalismasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasarimCalismasiApp.swift
+//  TasarimCalismasi
+//
+//  Created by Nevin Özkan on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TasarimCalismasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
